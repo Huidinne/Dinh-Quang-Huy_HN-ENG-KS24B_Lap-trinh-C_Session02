5ad756467ced0038bde4c487cc9224fee22ebd8a
Huidinne <quanghuy432006@gmail.com>
2024-11-19 20:37:50 +0700

    Upload btvn Session2_b1

---
+++ ss2_b1.cpp
@@ -0,0 +1,14 @@
+int main(){
+	// khai bao va khoi tao cac bien voi cac kieu du lieu
+	
+	// kieu int: luu tru cac so nguyen
+	int age=25;
+	
+	// kieu float: Luu tru cac so thuc
+	float pi= 3.14;
+	
+	// kieu char: luu tru mot ki tu
+	char sex = 'M';
+	
+	// kieu logic dung de luu gia tri true hoac false
+}
