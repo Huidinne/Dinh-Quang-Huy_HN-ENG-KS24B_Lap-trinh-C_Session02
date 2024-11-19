f8c24ce64eb10a4d4d5f26508f51fb7e779a6d4c
Huidinne <quanghuy432006@gmail.com>
2024-11-19 20:38:54 +0700

    Upload btvn Session2_b5

---
+++ ss2_b5.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main(){
+ 	int length = 10;
+ 	int width = 5;
+ 	printf(" Chieu dai và chieu rong cua hcn lan luot la: %d va %d\n",length,width);
+ 	
+ 	int perimeter = (length + width) * 2;
+	int acreage = length * width;
+	printf("Chu vi cua hcn la:%d\n",perimeter);
+	printf("Dien tich cua hcn la:%d\n",acreage);
+	
+return 0;
+ }
