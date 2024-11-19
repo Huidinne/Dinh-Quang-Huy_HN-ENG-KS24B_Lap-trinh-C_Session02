2d749a5371b21b604b0cb1e39aa593fe2cd8d9f9
Huidinne <quanghuy432006@gmail.com>
2024-11-19 20:38:42 +0700

    Upload btvn Session2_b4

---
+++ ss2_b4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ int main(){
+ 	int side = 5;
+ 	printf("Canh cua hinh vuong la: %d\n",side);
+ 	
+ 	int chu_vi = side * 4;
+ 	int dien_tich = side * side;
+ 	
+ 	printf("Chu vi hinh vuong là: %d\n",chu_vi);
+ 	printf("Dien tich hinh vuong là: %d\n",dien_tich);
+ 	
+ return 0;	
+ }
